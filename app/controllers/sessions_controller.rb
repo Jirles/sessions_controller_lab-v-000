1a3beccd6120486473f0c3bae863fa13ffd1d38f
Emily Jirles <emily.jirles@gmail.com>
2018-06-21 19:38:19 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    sessions[:name] = params[:name]
+    redirect_to root_path
   end
 
   def destroy
 
   end
-  
+
 end
