35edc78dc979933262071befe7d4c7da4ea496f5
Emily Jirles <emily.jirles@gmail.com>
2018-06-21 19:17:12 +0000

    automatically backed up by learn

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-describe 'Sessions Features' do 
-  context 'login interface' do 
-    it 'renders a view with a form with a name text field' do 
+describe 'Sessions Features' do
+  context 'login interface' do
+    it 'renders a view with a form with a name text field' do
       visit '/'
       click_link "Login"
 
       expect(page.status_code).to eq(200)
       expect(page).to have_selector('form')
-      expect(page).to have_text_field(:name)
-    end 
-  end 
+      expect(page).to have_field(:name)
+    end
+  end
 
-end 
+end
